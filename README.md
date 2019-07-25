a82f1e87b64161194f9c6304cbf1b27408dfc9f3
Yegor Bugayenko <yegor256@gmail.com>
2019-07-25 21:39:48 +0300

    new code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 This is a simple add-on for Firefox, which helps with automatic
 proxy authorization in a Selenium/Geckodriver testing.
 
-https://stackoverflow.com/questions/29735062/firefox-extension-set-proxy-auth
+See: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/webRequest/onAuthRequired
--- auth.js
+++ auth.js
@@ -1,18 +1,12 @@
-var prefs = Components.classes['@mozilla.org/preferences-service;1']
-  .getService(Components.interfaces.nsIPrefBranch);
-var host = pref.getCharPref('extensions.quickproxyauth.host');
-var port = pref.getCharPref('extensions.quickproxyauth.port');
-var login = pref.getCharPref('extensions.quickproxyauth.login');
-var password = pref.getCharPref('extensions.quickproxyauth.password');
-var Ctor = new Components.Constructor(
-  '@mozilla.org/login-manager/loginInfo;1',
-  Components.interfaces.nsILoginInfo,
-  'init'
+browser.webRequest.onAuthRequired.addListener(
+  function (details) {
+    return {
+      authCredentials: {
+        username: "QEtcUh",
+        password: "37gnqu"
+      }
+    };
+  },
+  { urls: ["<all_urls>"] },
+  ["blocking"]
 );
-var info = new Ctor(
-  host, null, 'moz-proxy://' + host + ':' + port,
-  login, password, '', ''
-);
-var manager = Components.classes['@mozilla.org/login-manager;1']
-  .getService(Components.interfaces.nsILoginManager);
-manager.addLogin(info);
