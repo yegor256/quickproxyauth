c8fe72a3c54deba81d6f502d89441530bf6bf9b2
Yegor Bugayenko <yegor256@gmail.com>
2019-07-26 23:55:53 +0300

    cookie

--- auth.js
+++ auth.js
@@ -1,12 +1,15 @@
-browser.webRequest.onAuthRequired.addListener(
-  function (details) {
-    return {
-      authCredentials: {
-        username: "QEtcUh",
-        password: "37gnqu"
-      }
-    };
-  },
-  { urls: ["<all_urls>"] },
-  ["blocking"]
-);
+browser.cookies.get({ url: 'http://quickproxyauth.com', name: 'credentials'}).then(function (cookie) {
+  var parts = cookie.value.split(':');
+  browser.webRequest.onAuthRequired.addListener(
+    function (details) {
+      return {
+        authCredentials: {
+          username: parts[0],
+          password: parts[1]
+        }
+      };
+    },
+    { urls: ["<all_urls>"] },
+    ["blocking"]
+  );
+});
